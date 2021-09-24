93eaf8d71a9ef309eb9e6ab877209fe05191a5b3
Diogo Santana <58599175+diogosantana2011@users.noreply.github.com>
2021-09-24 16:35:23 +0200

    (fix):amend-imput-to-input
    fixed typo

--- Projects/Credit Card/CreditCardAcceptance.py
+++ Projects/Credit Card/CreditCardAcceptance.py
@@ -1,8 +1,8 @@
-Name = string (imput("Enter name here: "))
+Name = string (input("Enter name here: "))
 
 Salary = float(input("Please enter salary here: €"))
 
-careerLength = int (imput("Years contributed to job:"))
+careerLength = int (input("Years contributed to job:"))
 
 if Salary > 30000 or (Salary > 20000 and careerLength > 3):
 print("You are entitled to a credit-card.")
